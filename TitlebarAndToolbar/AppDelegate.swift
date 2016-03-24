db9722f48319a2c55c1a29c5a68d9cfcb3382bd4
Lu Yibin <robinlu@me.com>
2016-03-24 12:05:44 +0800

    Initial Commit

---
+++ TitlebarAndToolbar/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  TitlebarAndToolbar
+//
+//  Created by Lu Yibin on 16/3/24.
+//  Copyright © 2016年 Lu Yibin. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ TitlebarAndToolbar/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TitlebarAndToolbar
+//
+//  Created by Lu Yibin on 16/3/24.
+//  Copyright © 2016年 Lu Yibin. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
